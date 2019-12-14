ebd9b0723a49914479b1c29328e7996ca71dba25
Lucas Eduardo <lucas59356@gmail.com>
2019-12-14 13:44:03 -0300

    Tentativa 5

--- constants.c
+++ constants.c
@@ -1,4 +1,4 @@
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
-const int FPS = 30;
-const float tick_size = 1/FPS;
+int SCREEN_WIDTH = 800;
+int SCREEN_HEIGHT = 600;
+int FPS = 30;
+float tick_size = 1/FPS;
